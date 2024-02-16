8bbfb87100c4edeb58cbf084b71d778051429d09
Lou <102719202+LouHV@users.noreply.github.com>
2024-02-16 09:54:25 +0700

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,13 +19,55 @@ const register = asyncHandler(async (req, res) => {
             message: 'Input is Email',
         })
     }
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        success: response ? true : false,
-        data: response
-    })
+
+    // kiem tra neu ton tai email tra ra loi
+    const user = await User.findOne({ email })
+    if (user)
+        throw new Error("User has existed!")
+    else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success: newUser ? true : false,
+            data: newUser,
+            message: newUser ? 'Register is successfully' : 'Something went wrong'
+        })
+    }
 })
 
-module.exports = { 
-    register, 
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    // kiem tra co la @gmail.com khong
+    const reg = /^(.+)@(\S+)$/
+    const isCheckEmail = reg.test(email)
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Input is required',
+        })
+    }
+    else if (!isCheckEmail) {
+        return res.status(400).json({
+            success: false,
+            message: 'Input is Email',
+        })
+    }
+
+    // logic dang nhap
+    // plain object
+    const response = await User.findOne({ email })
+    if (response && await response.isCorrectPassword(password)) {
+        const { password, role, ...userData } = response.toObject()
+        return res.status(200).json({
+            success: true,
+            userData
+        })
+    } else {
+        throw new Error('Invalid crendentials!')
+    }
+})
+
+module.exports = {
+    register,
+    login,
 }
