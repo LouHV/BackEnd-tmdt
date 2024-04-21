21cf76060a2fc3fc9a22035363cf7d4119829cae
luuluu-ptit <congluu.ptit@gmail.com>
2024-04-21 23:31:12 +0700

    save drag

--- Server/controllers/cartController.js
+++ Server/controllers/cartController.js
@@ -51,7 +51,6 @@ const addProductToCart = asyncHandler(async (req, res) => {
         cart.cart_products[productIndex].price += price;
     } else {
         const product = await Product.findById(productId);
-        console.log('product :>> ', product);
         if (!product) {
             return res.status(404).json({ success: false, message: 'Product not found' });
         }
