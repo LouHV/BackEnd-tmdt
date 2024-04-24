154469ee15006cd5573aa5d3f6216e1b0301c132
luuluu-ptit <congluu.ptit@gmail.com>
2024-04-24 23:49:20 +0700

    save drag

--- Client/src/components/common/Momo.js
+++ Client/src/components/common/Momo.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import axios from 'axios'
 import { apiCreateOrder, createPayment } from "../../apis";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+
 
 const MoMoPayment = ({ amount, payload, setIsSuccess }) => {
+
+    const navigate = useNavigate()
+
 
     const {
         products,
@@ -14,49 +22,56 @@ const MoMoPayment = ({ amount, payload, setIsSuccess }) => {
         coupon_code
     } = payload;
 
-    const returnUrl = 'http://localhost:3000/';
-    const orderInfo = 'Cửa hàng Shop Lou';
-    const orderId = '6626040459d8daa53b394999';
+    var date = new Date().getTime();
 
+    const requestId = date + "id";
+    const orderId = date + ":0123456778";
+    const requestType = 'payWithATM';
+    // const notifyUrl = 'https://sangle.free.beeceptor.com';
+    // const notifyUrl = 'https://sangle.free.beeceptor.com';
+    const notifyUrl = 'http://localhost:3000';
+    const returnUrl = 'http://localhost:3000';
+    // const amount = amount;
+    const orderInfo = "Thanh toán qua ví MoMo";
+    const extraData = "ew0KImVtYWlsIjogImh1b25neGRAZ21haWwuY29tIg0KfQ==";
 
     const handlePayment = async () => {
-        // try {
-        //     const response = await createPayment({ orderId: orderId, amount: amount, orderInfo: orderInfo, returnUrl: returnUrl })
-        //     if (!response.ok) {
-        //         throw new Error('Failed to create payment');
-        //     }
-        //     const result = response;
-
-        //     setIsSuccess(true);
-        //     const res = await apiCreateOrder(payload);
-
-        // } catch (error) {
-        //     console.error('Error:', error);
-        // }
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                partnerCode: 'MOMOBKUN20180529',
-                partnerName: "Test",
-                storeId:"MOMOBKUN20180529",
-                requestType: "payWithATM",
-                ipnUrl : "https://sangle.free.beeceptor.com",
-                redirectUrl : "https://sangle.free.beeceptor.com",
-                orderId: "1642387834078:0123456778",
-                amount:50000,
-                orderInfo: orderInfo,
-                lang:"vi",
-                requestId:"1713890784040id",
-                extraData:"ew0KImVtYWlsIjogImh1b25neGRAZ21haWwuY29tIg0KfQ==",
-                signature:"19b6bae640dd1ddb7f7dcf95d96d9f1df42930d4d15f49708ba230ce48d7baf5"
+        try {
+            const response = await createPayment({
+                requestId,
+                orderId,
+                requestType,
+                notifyUrl,
+                returnUrl,
+                amount,
+                orderInfo,
+                extraData
             })
-            // body: { status: status }
-        };
+            if (!response) {
+                throw new Error('Failed to create payment');
+            }
 
-        const response = await fetch(`https://test-payment.momo.vn/v2/gateway/api/create`, requestOptions);
-        const result = await response.json();
-        console.log('object :>> ', result);
+            if (response.success) {
+                setIsSuccess(true);
+
+                console.log("XXXX", response.payUrl);
+
+                window.open(`${response.payUrl}`, "_blank");
+
+                // navigate(`${response.payUrl}`);
+            }
+
+            // const res = await apiCreateOrder(payload);
+            // setTimeout(() => {
+            //     Swal.fire('Congrat!', 'Order was created.', 'success').then(() => {
+            //         navigate('/')
+            //         window.close()
+            //     })
+            // }, 1500)
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
--- Server/controllers/paymentController.js
+++ Server/controllers/paymentController.js
@@ -1,105 +1,96 @@
-const { MomoPayment } = require('momo-payment-gateway');
+const fetch = require('node-fetch');
+const CryptoJS = require('crypto-js');
 
-/* HOST_WEBHOOK => Partner API. Used by MoMo to submit payment results by IPN method (server-to-server) method */
-const HOST_WEBHOOK = process.env.HOST_WEBHOOK;
+
+const asyncHandler = require('express-async-handler');
 
 const partnerCode = process.env.PARTNERCODE;
 const accessKey = process.env.ACCESSKEY;
 const secretKey = process.env.SECRETKEY;
-const apiEndpoint = process.env.APIENDPOINT;
-console.log('partnerCode :>> ', partnerCode);
-class MomoPaymentController {
-    constructor(partnerCode, accessKey, secretKey, apiEndpoint) {
-        this.momoPayment = new MomoPayment({
-            partnerCode,
-            accessKey,
-            secretKey,
-            apiEndpoint,
-        });
-    }
 
-    async createPayment({
-        partnerCode,
-        orderId,
-        amount,
-        orderInfo,
-        returnUrl
-    }) {
-        console.log(partnerCode,orderId, amount, orderInfo, returnUrl, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX1");
-        try {
-            console.log(orderId, amount, orderInfo, returnUrl, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2");
-            if (!orderId || !amount || !returnUrl || !orderInfo) {
-                throw new Error('invalid input');
-            }
-            const result = await this.momoPayment.createPayment({
-                partnerCode,
-                requestId: `ID-${orderId}-${Math.round(Date.now() / 1000)}`,
-                orderId: `${orderId}-${Math.round(Date.now() / 1000)}`,
-                amount,
-                orderInfo,
-                returnUrl,
-                ipnUrl: 'http://localhost:3000/',
-            });
-            return result;
-        } catch (error) {
-            console.error(error)
-            throw error;
-        }
-    }
-
-    async refundPayment({ requestId, orderId, amount, transId }) {
-        try {
-            if (!orderId || !amount || !transId) {
-                throw new Error('invalid input');
-            }
-            const result = await this.momoPayment.refundPayment({
-                requestId,
-                orderId,
-                amount,
-                transId,
-            });
-            return result.data;
-        } catch (error) {
-            console.error(error)
-            throw error;
-        }
-    }
-
-    verifySignature({
-        signature,
+const createPayment = asyncHandler(async (req, res) => {
+    // const { _id } = req.user;
+    const {
         requestId,
         orderId,
+        requestType,
+        notifyUrl,
+        returnUrl,
         amount,
         orderInfo,
-        orderType,
-        transId,
-        message,
-        localMessage,
-        responseTime,
-        errorCode,
-        payType,
-    }) {
-        try {
-            const result = this.momoPayment.verifySignature({
-                signature,
-                requestId,
-                orderId,
-                amount,
-                orderInfo,
-                orderType,
-                transId,
-                message,
-                localMessage,
-                responseTime,
-                errorCode,
-                payType,
-            });
-            return result;
-        } catch (error) {
-            console.error(error)
-            throw error;
-        }
-    }
-}
+        extraData
+    } = req.body;
 
-module.exports = new MomoPaymentController(partnerCode, accessKey, secretKey, apiEndpoint);
+    const variables = {
+        requestId: requestId,
+        orderId: orderId,
+        requestType: requestType,
+        notifyUrl: notifyUrl,
+        returnUrl: returnUrl,
+        amount: amount,
+        orderInfo: orderInfo,
+        extraData: extraData,
+        partnerCode: partnerCode,
+        accessKey: accessKey,
+        secretKey: secretKey,
+    };
+
+    for (const [key, value] of Object.entries(variables)) {
+        console.log(`${key}: ${value}`);
+    }
+
+    var signature = "accessKey=" + accessKey
+        + "&amount=" + amount + "&extraData=" + extraData
+        + "&ipnUrl=" + notifyUrl + "&orderId=" + orderId
+        + "&orderInfo=" + orderInfo + "&partnerCode="
+        + partnerCode + "&redirectUrl="
+        + returnUrl + "&requestId=" + requestId
+        + "&requestType=" + requestType;
+
+    const hash = CryptoJS.HmacSHA256(signature, secretKey);
+    signature = CryptoJS.enc.Hex.stringify(hash);
+
+    const payload = {
+        partnerCode,
+        partnerName: "Test",
+        storeId: partnerCode,
+        requestType,
+        ipnUrl: notifyUrl,
+        redirectUrl: returnUrl,
+        orderId,
+        amount,
+        lang: 'vi',
+        orderInfo,
+        requestId,
+        extraData,
+        signature,
+    }
+
+    const requestEndpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }
+
+    try {
+        const response = await fetch(requestEndpoint, fetchOptions);
+        const jsonResponse = await response.json();
+
+        console.log("jsonResponseXXXX:::", jsonResponse);
+
+        return res.json({
+            success: jsonResponse ? true : false,
+            jsonResponse: jsonResponse ? jsonResponse : 'Something went wrong'
+        })
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
+})
+
+
+module.exports = {
+    createPayment
+}
